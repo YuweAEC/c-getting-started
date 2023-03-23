97867350e33a1c62aeb7211faa3c59e452f92073
Yuvraj Singh <yuwe18b@gmail.com>
2023-03-24 00:11:47 +0530

    modifying my loopsin the file

--- basics-in-c/loops.c
+++ basics-in-c/loops.c
@@ -2,17 +2,9 @@
 int main()
 {
     int i;
-<<<<<<< HEAD
-    for(i=1; i<=5; i++)
-=======
     for(i=1; i<=10; i++)
->>>>>>> 9e1bd6786c7d376aee349b366c69cd6a22eef087
     {
         printf("\n Hello World");
     }
     return 0;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9e1bd6786c7d376aee349b366c69cd6a22eef087
