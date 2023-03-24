929f5172aa4db615ade6a4caa979430cf07329f9
Yuvraj Singh <yuwe18b@gmail.com>
2023-03-24 09:14:27 +0530

    adding  comments

--- .vscode/practise.c
+++ .vscode/practise.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-void main()
-{
-    int a,b;
-    a= -3 - - 3;
-    b= -3 - - (-3);
-    printf("a=%d b=%d", a, b);
-}
+// void main()
+// {
+//     int a,b;
+//     a= -3 - - 3;
+//     b= -3 - - (-3);
+//     printf("a=%d b=%d", a, b);
+// }
 
 
 void main()
