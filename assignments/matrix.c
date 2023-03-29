d3beffb0ad0e969df64660107e6fa4a5cdb41da5
Yuvraj Singh <yuwe18b@gmail.com>
2023-03-29 22:08:21 +0530

    adding new codes

--- assignments/matrix.c
+++ assignments/matrix.c
@@ -8,15 +8,27 @@ void main()
     int matrix[m][n];
     // int arr[4][3]={{1,2,3},{2,3,4},{3,4,5},{4,5,6}};
     //traversing 2D array
+
     for(i=0;i<m;i++)
+}
  {
- for(j=0;j<n;j++)
-    {
-      printf("Enter values for : matrix[%d] [%d] \n",i+1,j+1);
-      scanf("%d",&matrix[i][j]);
+    for(j=0;j<n;j++)
+    {
+        printf("Enter values for : matrix[%d] [%d] \n",i+1,j+1);
+        scanf("%d",&matrix[i][j]);
+    }
 
-    }
-  }
+ }
 
 
+  printf("\n printing the elements ....\n");
+  for(i=0;i<m;i++)
+  {
+
+        printf("\n");
+        for (j=0;j<n;j++)
+        {
+            printf("%d\t",matrix[i][j]);
+         }
+   }
 
