0fd8d4824a1f6f7b0311df02a87fd3b49d2b6426
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-03 20:49:12 +0530

    adding new codes

--- assignments/no1.c
+++ assignments/no1.c
@@ -3,11 +3,22 @@
 void main()
 {
     int i, j, k, l, n;
-    printf("Enter a number of rows:\n");
-    scanf("%d",%n);
+    printf("Enter the no. of rows:\n");
+    scanf("%d",&n);
 
-    for (i=n;i>=1;i--)
+    for(i=n;i>=1;i--)
     {
+        for(k=n;k>=i;k--)
+        {
+            printf(" ");
+
+        }
         
-    }
+        for(j=1;j<=i;j++);
+        {
+            printf("%d",j);
+
+        }
+    printf("\n")
+
 }
