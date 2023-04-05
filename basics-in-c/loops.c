8772769e25db01c03c3383143505ca099207aff5
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-05 21:52:07 +0530

    adinng commnets

--- basics-in-c/loops.c
+++ basics-in-c/loops.c
@@ -1,12 +1,13 @@
 /*WAP to print hello word using loops*/
 
 #include<stdio.h>
-int main()
-{
-    int i;
-    for(i=1; i<=10; i++)
-    {
-        printf("\n  Hello Wrold");
-    }
-    return 0;
-}
+// int main()
+// {
+//     int i;
+//     for(i=1; i<=10; i++)
+//     {
+//         printf("\n  Hello Wrold");
+//     }
+//     return 0;
+// }
+
