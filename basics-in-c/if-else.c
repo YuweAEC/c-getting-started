01fd958f1fc917c22b87d12322f0bde5a40567d2
Yuvraj Singh <117657410+YuweAEC@users.noreply.github.com>
2023-04-06 16:04:41 +0530

    adding new odes

--- basics-in-c/if-else.c
+++ basics-in-c/if-else.c
@@ -10,12 +10,19 @@
 
 #include<stdio.h>
 
-void main()
+int main()
 {
-    float d;
-    float atp;
-    printf("Enter Distance Travelled : ");
-    scanf("%f",%d);
+    float d,A,B,C;
+    printf("Enter Distance Travelled: ");
+    scanf("%d",%d);
+    
+    if(0<=d && d<=100)
+        {
+            A=((d*13.25));
+            printf("Amount charged :%f", A);
+    }
+    
+    else if (d<=250)
     
     
 }
