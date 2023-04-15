564eccda14c97a2352730ef28f7322e4b20929b1
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-15 12:11:31 +0530

    adding new file with some codes in practi`ce file

--- basics-in-c/practise.c
+++ basics-in-c/practise.c
@@ -8,13 +8,13 @@
 // }
 
 #include<stdio.h>
-// void main()
-// {
-//     int a,b;
-//     printf("Enter values of a and b");
-//     scanf("%d %d",&a,&b);
-//     printf("a==%d b==%d",a,b);
-// }
+void main()
+{
+    int a,b;
+    printf("Enter values of a and b");
+    scanf("%d %d",&a,&b);
+    printf("a==%d b==%d",a,b);
+}
 
 
 int main()
