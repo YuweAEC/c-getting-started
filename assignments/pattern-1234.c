40c4eb2876ef1f37dd8df5d60f7d7d24fd763012
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-20 15:03:16 +0530

    adding new pattern

--- assignments/pattern-1234.c
+++ assignments/pattern-1234.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
    int i, j, rows;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = 1; i <= rows; ++i) {
-      for (j = 1; j <= i; ++j) {
+   for (i = 1; i <= rows; ++i) 
+   {
+      for (j = 1; j <= i; ++j) 
+      {
          printf("%d ", j);
       }
       printf("\n");
