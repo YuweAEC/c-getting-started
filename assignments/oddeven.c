f1ecc0561c18b727d34cd68dd102262c7b7ef988
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-21 21:06:17 +0530

    adding comments

--- assignments/oddeven.c
+++ assignments/oddeven.c
@@ -1,25 +1,25 @@
-/**
- * C program to check Even or Odd number using switch case
- */
+// /**
+//  * C program to check Even or Odd number using switch case
+//  */
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main()
-{
-    int num;
-    printf("Enter number to check even/odd:");
-    scanf("%d", &num);
-    switch(num % 2)
-    {
-        case 0:
-        printf("Number is Even:");
-        break;
-        case 1: 
-        printf("Number is Odd:");
-        break;
-    }
-    return 0;
-}
+// int main()
+// {
+//     int num;
+//     printf("Enter number to check even/odd:");
+//     scanf("%d", &num);
+//     switch(num % 2)
+//     {
+//         case 0:
+//         printf("Number is Even:");
+//         break;
+//         case 1: 
+//         printf("Number is Odd:");
+//         break;
+//     }
+//     return 0;
+// }
 
 
 #include<stdio.h>
