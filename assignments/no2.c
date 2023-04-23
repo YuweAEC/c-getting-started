ab97db17e75e800812f7ac2a45ee506aee495830
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-23 19:47:23 +0530

    add new codes

---
+++ assignments/no2.c
@@ -0,0 +1,19 @@
+/*
+ * C Program to print Multiplication table using for loop
+ */
+ 
+#include <stdio.h>
+ 
+int main()
+{
+    int n, i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Multiplication table of %d:\n ", n);
+    printf("--------------------------\n");
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+    return 0;
+}
