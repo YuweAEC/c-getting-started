7919ef9facc9735219db9a2a41a4e21edc8a1ab2
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-23 19:51:31 +0530

    add new codes to same file

--- assignments/no2.c
+++ assignments/no2.c
@@ -20,3 +20,14 @@ int main()
 }
 */
 
+#include <stdio.h>
+int main() {
+  int n;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+
+  for (int i = 1; i <= 10; ++i) {
+    printf("%d * %d = %d \n", n, i, n * i);
+  }
+  return 0;
+}
