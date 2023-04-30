6dfb58fdab8001e0fdc50fb055d330ea49916a8e
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-30 13:50:41 +0530

    add new lines of code

--- basics-in-c/if-else.c
+++ basics-in-c/if-else.c
@@ -26,3 +26,14 @@
     
     
 // }
+
+void main()
+{
+    int num;
+
+    printf("Enter a number less than 10");
+    scanf("%d", &num);
+
+    if (num<10)
+    printf("What an obideant servant you are !");
+}
