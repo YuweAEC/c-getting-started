e6529dc4c129d7405523e5359772ab24af4de4d6
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-30 15:04:22 +0530

    add comment to codes

--- basics-in-c/if-else.c
+++ basics-in-c/if-else.c
@@ -20,14 +20,14 @@ int main()
     
 }
  
-void main()
-{
-    int num;
+// void main()
+// {
+//     int num;
 
-    printf("Enter a number less than 10");
-    scanf("%d", &num);
+//     printf("Enter a number less than 10");
+//     scanf("%d", &num);
 
-    if (num<=10)
-    printf("What an obideant servant you are !");
-}
+//     if (num<=10)
+//     printf("What an obideant servant you are !");
+// }
 
