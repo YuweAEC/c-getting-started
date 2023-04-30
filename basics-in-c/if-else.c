c5590f9bfb290c0d8041a99817ca3ab2c40d3419
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-30 12:06:39 +0530

    add comment to my code

--- basics-in-c/if-else.c
+++ basics-in-c/if-else.c
@@ -10,19 +10,19 @@
 
 #include<stdio.h>
 
-int main()
-{
-    float d,A,B,C;
-    printf("Enter Distance Travelled: ");
-    scanf("%d",%d);
+// int main()
+// {
+//     float d,A,B,C;
+//     printf("Enter Distance Travelled: ");
+//     scanf("%d",%d);
     
-    if(0<=d && d<=100)
-        {
-            A=((d*13.25));
-            printf("Amount charged :%f", A);
-    }
+//     if(0<=d && d<=100)
+//         {
+//             A=((d*13.25));
+//             printf("Amount charged :%f", A);
+//     }
     
-    else if (d<=250)
+//     else if (d<=250)
     
     
-}
+// }
