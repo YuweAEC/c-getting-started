5fb3c7ba976b13d5fcb3cbc415e12f453f9bad62
Yuvraj Singh <yuwe18b@gmail.com>
2023-05-01 15:21:40 +0530

    add new codes to assignment

---
+++ assignments/no3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float d, A;
+    printf("Enter Distance Travelled: ");
+    scanf("%f", &d);
+
+    if (0 <= d && d <= 100)
+    {
+        A = (d * 13.25);
+        printf("Amount charged :%f", A);
+    }
+    else
+    {
+        printf("Not getting the right amount:");
+    }
+}
