a4d3b67ebea78b6fde17dbb10c631ea08ad2ac99
Yuvraj Singh <yuwe18b@gmail.com>
2023-05-01 15:24:10 +0530

    remove codes from here

--- basics-in-c/if_else.c
+++ basics-in-c/if_else.c
@@ -1,22 +1,4 @@
-#include<stdio.h>
 
-int main()
-{
-    float d,A;
-    printf("Enter Distance Travelled: ");
-    scanf("%f",&d);
-    
-    if(0<=d && d<=100)
-    {
-        A=(d*13.25);
-        printf("Amount charged :%f", A);
-    }
-    else
-    {
-        printf("Not getting the right amount:");
-    }
-
-}
 
 // void main()
 // {
