ae631dd04b2305344e76f8ce64ebfc84e82dea85
Yuvraj Singh <yuwe18b@gmail.com>
2023-05-21 22:54:47 +0530

    add main func codes

--- functions and pointers/recursion.c
+++ functions and pointers/recursion.c
@@ -2,3 +2,13 @@
 
 void factorial(int);
 
+void main()
+{
+    int a, fact;
+
+    printf("\n Enter a number:");
+    scanf("%d", &a);
+
+    fact = factorial(a);
+    printf("factorial value =%d", fact);
+}
