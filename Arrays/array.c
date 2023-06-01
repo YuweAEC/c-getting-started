df0e30a731c970e082c70e1e746c168ff3a81eff
Yuvraj Singh <117657410+YuweAEC@users.noreply.github.com>
2023-06-01 11:48:42 +0530

    add new codes to file

--- Arrays/array.c
+++ Arrays/array.c
@@ -5,14 +5,14 @@ int main()
 {
     int avg, sum = 0;
     int i;
-    int marks[30]; /* array declaration */
-    for (i = 0; i <= 29; i++)
+    int marks[8]; /* array declaration */
+    for (i = 0; i <= 7; i++)
     {
         printf("\nEnter marks ");
         scanf("%d", &marks[i]); /* store data in array */
     }
-    for (i = 0; i <= 29; i++)
+    for (i = 0; i <= 7; i++)
         sum = sum + marks[i]; /* read data from an array*/
-    avg = sum / 30;
+    avg = sum / 8;
     printf("\nAverage marks = %d", avg);
 }
