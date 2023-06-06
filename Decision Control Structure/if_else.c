effb69d904a5753b381c24bb5e91d61eca123349
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-06 23:40:35 +0530

    add new code to the file with some comments

--- Decision Control Structure/if_else.c
+++ Decision Control Structure/if_else.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
-
+/*
 void main(){
 
     int num;
@@ -17,5 +17,23 @@ void main(){
     printf("/nStay follow the rules");
 
 }
+
+*/
+
+int main()
+{
+    int x= 5, y;
+    
+    y= (x==4 ? 24 : 32);
+
+    printf("\n address of x = %d", &x);
+    printf("\n address of y = %d", &y);
+
+    printf("\n Value of x = %d", *(&x));
+    printf("\n Value of y = %d", *(&y));
+}
+
+// int x = 6;
+// int y = (x == 5 ? 5 : 10);
 
 
